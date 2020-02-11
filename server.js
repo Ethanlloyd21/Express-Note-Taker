6cd328b88348dd5a155b48fca502e3634f765441
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-02-10 17:30:41 -0800

    added fs, util and writefile

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const writeFiles = util.promisify(fs.writeFile);
 const app = express();
 
 
