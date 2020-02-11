ebcf47cc43cc6563842f589ad3831de69d0abe62
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-02-10 17:28:41 -0800

    added get  /notes and /*

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
 
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'templates', 'notes.html'));
 })
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'templates', 'index.html'));
+})
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
