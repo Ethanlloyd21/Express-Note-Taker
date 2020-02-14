a4c6d70700257c5094bbcd07196f1a96422f7b8c
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-02-14 02:54:41 -0800

    update

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const uuid = require('uuid');
 const router = express.Router();
-const fs = require('fs');
 const db = require('../../db/Db');
 
 // Gets all text in db json file
