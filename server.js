f6926c54e3447e7f9dd219eda71454fb350090a6
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-02-14 08:20:21 -0800

    update

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(404).send('Something broke!')
+});
+
 // Have the style.css works for the html
 app.get('/notes/css', (req, res) => res.sendFile(path.join(__dirname, './public/assets/css/style.css')));
 // Have the index.js works for the html
@@ -15,6 +20,8 @@ app.get('/notes/js', (req, res) => res.sendFile(path.join(__dirname, './public/a
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
 // Get routes for index.html
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+
+
 
 // notes API routes
 app.use('/api/notes', require('./routes/api/notes'));
